f809a047c6f9ddddcb92f3f8349f9b989b53bea8
robertbutacu <robertbutacu@gmail.com>
2018-10-01 15:39:23 +0100

    mongo factory and converter

--- money-track-backend/src/main/scala/data/Transaction.scala
+++ money-track-backend/src/main/scala/data/Transaction.scala
@@ -1,5 +1,23 @@
 package data
 
+import java.text.SimpleDateFormat
 import java.util.Date
 
+import com.mongodb.casbah.Imports._
+
 case class Transaction(name: String, category: Option[String] = None, amount: BigDecimal, date: Date)
+
+object Common {
+  /**
+    * Convert a Stock object into a BSON format that MongoDb can store.
+    */
+  def buildMongoDbObject(transaction: Transaction): MongoDBObject = {
+    val builder = MongoDBObject.newBuilder
+    builder += "name" -> transaction.name
+    builder += "category" -> transaction.category.getOrElse("")
+    builder += "amount" -> transaction.amount
+    builder += "date" -> new SimpleDateFormat("dd-mm-YYYY").format(transaction.date)
+
+    builder.result
+  }
+}
---
+++ money-track-backend/src/main/scala/server/MongoFactory.scala
@@ -0,0 +1,12 @@
+package server
+
+import com.mongodb.casbah.MongoConnection
+
+object MongoFactory {
+  private val SERVER = "localhost"
+  private val PORT   = 27017
+  private val DATABASE = "money-track"
+  private val COLLECTION = "transactions"
+  val connection = MongoConnection(SERVER)
+  val collection = connection(DATABASE)(COLLECTION)
+}
