c4f42ab88d872c2189fbe38df75a007b4bda6178
robertbutacu <robertbutacu@gmail.com>
2018-10-26 09:09:54 +0100

    refactoring -> doesnt work correctly for now

--- money-track-backend/src/main/scala/controllers/AmountController.scala
+++ money-track-backend/src/main/scala/controllers/AmountController.scala
@@ -8,54 +8,50 @@ import services.GETHandler
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 object AmountController extends Marshaller {
-  val amountRoutes: Route = path("amount") {
-    get {
-      parameter("date".as[String]) { date => {
-        val amount = GETHandler.getAmountSpentByDate(date)
+  val amountRoutes: Route =
+    path("amount") {
+      get {
+        parameter("date".as[String]) { date =>
+          val amount = GETHandler.getAmountSpentByDate(date)
 
-        complete(amount)
+          complete(amount)
+        }
+      } ~ path("interval") {
+        get {
+          parameter("start".as[String], "end".as[String]) { case (start, end) =>
+            val amount = GETHandler.getAmountSpentByPeriod(start, end)
+
+            complete(amount)
+          }
+        }
+      } ~ path("last") {
+        get {
+          parameter("days".as[Int]) { n =>
+            val amount = GETHandler.getAmountForLastNDays(n)
+
+            complete(amount)
+          }
+        }
+      } ~ get {
+        parameter("product".as[String]) { product =>
+          val amount = GETHandler.getAmountByProduct(product)
+
+          complete(amount)
+        }
+      } ~ get {
+        parameter("category".as[String]) { category =>
+          val amount = GETHandler.getAmountByCategory(category)
+
+          complete(amount)
+        }
       }
+    } ~ path("budget") {
+      get {
+        parameter("start".as[String], "end".as[String], "limit".as[Double]) { case (start, end, limit) =>
+          val remaining = GETHandler.getBudgetRemaining(start, end, limit)
+
+          complete(remaining)
+        }
       }
     }
-  } ~ path("amount" / "interval") {
-    get {
-      parameter("start".as[String], "end".as[String]) { case (start, end) =>
-        val amount = GETHandler.getAmountSpentByPeriod(start, end)
-
-        complete(amount)
-      }
-    }
-  } ~ path("amount" / "last") {
-    get {
-      parameter("days".as[Int]) { n =>
-        val amount = GETHandler.getAmountForLastNDays(n)
-
-        complete(amount)
-      }
-    }
-  } ~ path("budget") {
-    get {
-      parameter("start".as[String], "end".as[String], "limit".as[Double]) { case (start, end, limit) =>
-        val remaining = GETHandler.getBudgetRemaining(start, end, limit)
-
-        complete(remaining)
-      }
-    }
-  } ~ path("amount") {
-    get {
-      parameter("product".as[String]) { product =>
-        val amount = GETHandler.getAmountByProduct(product)
-
-        complete(amount)
-      }
-    }
-  } ~ path("amount") {
-    get {
-      parameter("category".as[String]) { category =>
-        val amount = GETHandler.getAmountByCategory(category)
-
-        complete(amount)
-      }
-    }
-  }
 }
--- money-track-backend/src/main/scala/controllers/TransactionController.scala
+++ money-track-backend/src/main/scala/controllers/TransactionController.scala
@@ -10,53 +10,50 @@ import models.Transaction
 import services.{GETHandler, POSTHandler}
 
 object TransactionController extends Marshaller {
-  val transactionRoutes: Route = path("transactions") {
-    get {
-      parameter("category".as[String]) { category =>
-        val transactions = GETHandler.getByCategory(category)
+  val transactionRoutes: Route =
+    path("transactions" /) {
+      get {
+        parameter("category".as[String]) { category =>
+          val transactions = GETHandler.getByCategory(category)
 
-        complete(StatusCodes.OK, transactions)
+          complete(StatusCodes.OK, transactions)
+        }
+      } ~
+        post {
+          entity(as[Transaction]) { t =>
+            POSTHandler.postTransaction(t)
+            complete(StatusCodes.Created)
+          }
+        } ~
+        get {
+          parameter("date".as[String]) { date =>
+            val transactions = GETHandler.getByDay(date)
+
+            complete(StatusCodes.OK, transactions)
+          }
+        } ~ pathSuffix("interval") {
+          get {
+            parameter("start".as[String], "end".as[String]) { case (start, end) =>
+              val transactions = GETHandler.getByPeriod(start, end)
+
+              complete(StatusCodes.OK, transactions)
+            }
+          }
+        } ~ get {
+        parameter("product".as[String]) { product =>
+          val transactions = GETHandler.getByProduct(product)
+
+          complete(StatusCodes.OK, transactions)
+
+        }
+      } ~ path("/last") {
+        get {
+          parameter("days".as[Int]) { n =>
+            val transactions = GETHandler.getForLastNDays(n)
+
+            complete(StatusCodes.OK, transactions)
+          }
+        }
       }
     }
-  } ~ path("transaction") {
-    post {
-      entity(as[Transaction]) { t =>
-        POSTHandler.postTransaction(t)
-        complete(StatusCodes.Created)
-      }
-    }
-  } ~ path("transactions") {
-    get {
-      parameter("date".as[String]) { date =>
-        val transactions = GETHandler.getByDay(date)
-
-        complete(StatusCodes.OK, transactions)
-      }
-    }
-  } ~ path("transactions" / "interval") {
-    get {
-      parameter("start".as[String], "end".as[String]) { case (start, end) =>
-        val transactions = GETHandler.getByPeriod(start, end)
-
-        complete(StatusCodes.OK, transactions)
-      }
-    }
-  } ~ path("transactions" / "last") {
-    get {
-      parameter("days".as[Int]) { n =>
-        val transactions = GETHandler.getForLastNDays(n)
-
-        complete(StatusCodes.OK, transactions)
-      }
-    }
-  } ~ path("transactions") {
-    get {
-      parameter("product".as[String]) { product =>
-        val transactions = GETHandler.getByProduct(product)
-
-        complete(StatusCodes.OK, transactions)
-
-      }
-    }
-  }
 }
